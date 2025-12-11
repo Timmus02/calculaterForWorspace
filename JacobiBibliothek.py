6728952a4738ae9260fc4906acce7e6c66a410eb
Tim Franzke <timfranzke2002@gmail.com>
2025-12-11 14:32:35 +0100

    Jacobi Matrix richtig berechnet mit JacobiBibliothek

---
+++ JacobiBibliothek.py
@@ -0,0 +1,84 @@
+from sympy import symbols, Matrix, cos, sin, pprint, trigsimp, pi
+import numpy as np
+
+class Cdh:
+    phi = np.empty(2)
+    alpha = np.empty(2)
+    a = 0
+    d = 0
+    phi, d, a, alpha = symbols('phi d a alpha')
+
+    def __init__(self, _phi:np.ndarray, _d, _a, _alpha: np.ndarray): #array of phi and alph should be ["value or symbol", Offset[int]]
+        self.alpha = _alpha
+        self.a = _a
+        self.d = _d
+        self.phi = _phi
+        self.calcTrans()
+
+    def calcTrans(self):
+        phi_offset, alpha_offset = symbols("phi_offset alpha_offset")
+        phi, phi_offset, alpha, alpha_offset, a, d = self.phi[0], self.phi[1], self.alpha[0], self.alpha[1] , self.a, self.d
+
+        phi_offset = self._convertToRad(phi_offset)
+        alpha_offset = self._convertToRad(alpha_offset)
+
+        if not type(phi) == np.str_:
+            phi = self._convertToRad(phi)
+        if not type(alpha) == np.str_:
+            alpha = self._convertToRad(alpha)
+
+        cPhi = self._AdditionstheoremCOS(phi, phi_offset)
+        sPhi = self._AdditionstheoremSIN(phi, phi_offset)
+        cAlpha = self._AdditionstheoremCOS(alpha, alpha_offset)
+        sAlpha = self._AdditionstheoremSIN(alpha, alpha_offset)
+        self.tran = Matrix([
+            [cPhi, -sPhi*cAlpha,  sPhi*sAlpha,  a*cPhi],
+            [sPhi,  cPhi*cAlpha, -cPhi*sAlpha,  a*sPhi],
+            [0,         sAlpha,           cAlpha,           d],
+            [0,         0,                    0,                    1]
+        ])
+ 
+    def getTrans(self):
+        return self.tran
+    def _AdditionstheoremCOS(self, x1, x2):
+        return cos(x1) * cos(x2) + -1 * sin(x1) * sin(x2)
+    def _AdditionstheoremSIN(self, x1, x2):
+        return sin(x1) * cos(x2) + cos(x1) * sin(x2)
+    def _convertToRad(self, deg):
+        #print(type(deg))
+        if type(deg) == np.float16 or type(deg) == np.int64:
+            return (pi/180)*deg
+        if type(deg) == np.str_:
+            if deg.isdigit():
+               return (pi/180)*float(deg) 
+        print("Wert nicht convertierbar in RAD")
+        exit()
+
+def calcJacobiRot(_0t, _0ta, TCPVec, flac): #0t is whole Transformation; a=i-1; TCPVec= Matrix([0, 0, 0, 1]); if flac 1 --> first Element ez0 = [0,0,1] 0r0=[0,0,0]
+    _0rE = _0t * TCPVec
+    _0rE.row_del(3)
+    if flac:
+        _0r0 = Matrix([0,0,0])
+        _0e0 = Matrix([0,0,1])
+        trans = _0e0.cross(_0rE-_0r0) #geht nur mit 3 Zeilen 
+        Or = _0e0
+        return trans, Or
+
+    _0ri = _0ta.col(3) #4 Spalte
+    _0ri.row_del(3)
+    #pprint(_0ri)
+    _0ei = _0ta.col(2) #3 Spalte
+    _0ei.row_del(3)
+
+    #pprint(_0ei)
+    trans = _0ei.cross(_0rE-_0ri) #geht nur mit 3 Zeilen 
+    Or = _0ei
+    #pprint(trans)
+    return trans, Or
+def clalcJacobiTrans(_0ta, flac): #flac 1 --> first Element ez0 = [0,0,1]
+    if flac:
+        return Matrix([0,0,1]), Matrix([0,0,0])
+    
+    _0ei = _0ta.col(2) #3 Spalte
+    _0ei.row_del(3)
+    return _0ei, Matrix([0, 0, 0])
--- jacobiSym.py
+++ jacobiSym.py
@@ -1,23 +1,6 @@
-from sympy import symbols, Matrix, cos, sin, pprint
+from sympy import symbols, Matrix, cos, sin, pprint, simplify
 import numpy as np
-from sympyCalcTransformationMatrix import Cdh
-
-def calcJacobiRot(_0t5, _0ta,): #a=i-1
-    TCPVec = Matrix([0, 0, 0, 1])
-    _0rE = _0t5 * TCPVec
-    _0rE.row_del(3)
-
-    _0ri = _0ta.col(3) #4 Spalte
-    _0ri.row_del(3)
-    pprint(_0ri)
-    _0ei = _0ta.col(2) #3 Spalte
-    _0ei.row_del(3)
-
-    pprint(_0ei)
-    trans = _0ei.cross(_0rE-_0ri) #geht nur mit 3 Zeilen 
-    Or = _0ei
-    pprint(trans)
-    return trans, Or
+from JacobiBibliothek import *
 
 l2 = "l2"
 
@@ -27,11 +10,11 @@ l4 = 600
 l5 = 600
 l6 = 300
 
-_0T1 = Cdh(0, l2, l1+l3, 0)
-_1T2 = Cdh("-q1", 0, l4, 0)
-_2T3 = Cdh("-q2", 0, l5, 0)
-_3T4 = Cdh("-q3+90", 0, 0, 90)
-_4T5 = Cdh("q4", l6, 0, 0)
+_0T1 = Cdh(np.array([0, 0]), l2, l1+l3, np.array([0, 0]))
+_1T2 = Cdh(np.array(["-q1", 0]), 0, l4, np.array([0, 0]))
+_2T3 = Cdh(np.array(["q2", 0]), 0, l5, np.array([0, 0]))
+_3T4 = Cdh(np.array(["q3", 90]), 0, 0, np.array([90, 0]))
+_4T5 = Cdh(np.array(["q4", 0]), l6, 0, np.array([0, 0]))
 _0T2 = _0T1.getTrans() * _1T2.getTrans()
 _0T3 = _0T2 * _2T3.getTrans()
 _0T4 = _0T3 * _3T4.getTrans()
@@ -50,30 +33,20 @@ print("######4T5######")
 pprint(_4T5.getTrans())
 
 print("######0T5######")
-pprint(_0T5)
+pprint(simplify(_0T5))
 
-##calc 0rE
-TCPVec = Matrix([0, 0, 0, 1])
-_0rE = _0T5 * TCPVec
-_0rE.row_del(3)
 #########Achse1##########################
-#_0e1 = _0T1.getTrans().col(2) #3 Spalte
-#_0e1.row_del(3)
-_0e1 = Matrix([0, 0, 0]) #kann das Stimmen? Heißt das die Schubachse keine Einfluss hat?
-trans1 = _0e1
-Or1 = Matrix([0, 0, 0])
+trans1, Or1 = clalcJacobiTrans("", 1)
 ###########Achse2########################
-trans2, Or2 = calcJacobiRot(_0T5, _0T1.getTrans())
+trans2, Or2 = calcJacobiRot(_0T5, _0T1.getTrans(), Matrix([0,0,0,1]), 0)
 #########Achse3########
-trans3, Or3 = calcJacobiRot(_0T5, _0T2)
+trans3, Or3 = calcJacobiRot(_0T5, _0T2,  Matrix([0,0,0,1]), 0)
 ###########Achse4#########
-trans4, Or4 = calcJacobiRot(_0T5, _0T3)
+trans4, Or4 = calcJacobiRot(_0T5, _0T3,  Matrix([0,0,0,1]), 0)
 ##########Achse5###########
-trans5, Or5 = calcJacobiRot(_0T5, _0T4)
+trans5, Or5 = calcJacobiRot(_0T5, _0T4,  Matrix([0,0,0,1]), 0)
 
+print("######Jacobi######")
 Jacobi = Matrix([ [trans1, trans2, trans3, trans4, trans5],
                   [Or1, Or2, Or3, Or4, Or5]])
-Jacobi.simplify
-print("Jacobi")
-pprint(Jacobi)
-print(Jacobi.shape)
+pprint(simplify(Jacobi))
